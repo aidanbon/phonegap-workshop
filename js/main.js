bcca2015000642955d8189778f525f8438c36371
Francis Au-Yeung <francis.auyeung@gmail.com>
2013-01-29 18:22:16 -0800

    Part 4 - Setting up a Single Page App.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 var app = {
 
+    initialize: function() {
+        var self = this;
+        this.store = new LocalStorageStore(function() {
+            self.renderHomeView();
+            self.showAlert("Store initialized", "Info");
+        });
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -21,14 +30,16 @@ var app = {
         }
     },
 
-    initialize: function() {
-        var self = this;
-        this.store = new LocalStorageStore(function() {
-            self.showAlert("Store initialized", "Info");
-        });
+    renderHomeView: function() {
+        var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>";
+        $('body').html(html);
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
-
 };
 
 app.initialize();
