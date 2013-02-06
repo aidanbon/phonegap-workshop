7026a95821645864a6ec8a75aea2c2dacde7eaf1
Francis Au-Yeung <francis.auyeung@gmail.com>
2013-02-05 21:08:44 -0800

    debug

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -3,16 +3,25 @@
 var EmployeeView = function(employee) {
  
     this.initialize = function() {
-        alert("in EmployeeView initialize");
+        this.showAlert("1,2", "in EmployeeView initialize");
         this.el = $('<div/>');
     };
  
     this.render = function() {
         this.el.html(EmployeeView.template(employee));
-        alert("render employee html "+this.el.html);
+        this.showAlert("1.3", "render employee html "+this.el.html);
         return this;
     };
 
+    this.showAlert = function (title, message) {
+        if (navigator.notification) {
+          navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    };
+
+    this.showAlert("1.1", "calling initialize");
     this.initialize();
  };
  
